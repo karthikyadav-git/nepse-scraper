4b655820043c5f9dceb5e1e12cd8f2db39d9919a
Karthik Yadav <mail@karthikyadav.com>
2025-01-15 13:31:58 +0545

    updated scraper tool to use databaseManager

--- tools/scraper.js
+++ tools/scraper.js
@@ -1,16 +1,16 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const Database = require('better-sqlite3');
+const DatabaseManager = require('./databaseManager');
 
-const db = new Database('./databases/stocks.db');
+const dbMan = new DatabaseManager('stocks.db')
 
 // separate scraped data array into individual stock array
-function separator(array, length) {
-  const stocksSeparated = [];
+function arrayByStock(array, length) {
+  const individualStocksArray = [];
   for(let i = 0; i < length; i += 10) {
-      stocksSeparated.push(array.slice(i, i+10));
+      individualStocksArray.push(array.slice(i, i+10));
   }
-  return stocksSeparated;
+  return individualStocksArray;
 }
 
 async function scraper() {
@@ -19,31 +19,15 @@ async function scraper() {
     const { data } = await axios.get('https://www.sharesansar.com/live-trading');
     const $ = cheerio.load(data);
 
-    const stockData = $('tbody tr td').text();
-    const stockArray = stockData.split(' ').filter(line => line !== '').join('').split('\n').filter(line => line !== '');
-    const separatedStocks = separator(stockArray, stockArray.length);
+    const rawStocksData = $('tbody tr td').text();
+    const stocksDataArray = rawStocksData.split(' ').filter(line => line !== '').join('').split('\n').filter(line => line !== '');
+    const dataArrayByStock = arrayByStock(stocksDataArray, stocksDataArray.length);
 
-    // create a database table
-    db.exec(`CREATE TABLE IF NOT EXISTS stockPrices(
-      id INTEGER,
-      symbol TEXT UNIQUE,
-      LTP REAL,
-      point_change REAL,
-      percent_change REAL,
-      open REAL,
-      high REAL,
-      low REAL,
-      volume INTEGER,
-      prev_close REAL
-      )`);
-    
-    // delete previous data table
-    db.exec(`DELETE FROM stockPrices`);
+    dbMan.createStocksTable();
+    dbMan.deleteStocksTable();
 
-    // store the stock info in the database table
-    const input = db.prepare(`INSERT INTO stockPrices(id, symbol, LTP, point_change, percent_change, open, high, low, volume, prev_close) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
-    for(let i = 0; i < separatedStocks.length; i++) {
-      input.run(separatedStocks[i]);
+    for(let i = 0; i < dataArrayByStock.length; i++) {
+      dbMan.insertStockData(dataArrayByStock[i]);
     }
 
     console.log("Stock prices updated successfully.");
